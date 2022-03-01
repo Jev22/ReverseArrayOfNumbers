184a1853008e09b614093e66031765a33bb04b21
Jev22 <50078304+Jev22@users.noreply.github.com>
2022-03-01 16:51:15 +0100

    v1.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,28 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int USER_INPUT_NUMBERS = 10;
+        int USER_INPUT_NUMBERS = 4;
 
         Double[] array = new Double[USER_INPUT_NUMBERS];
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter " + USER_INPUT_NUMBERS + " numbers: ");
+
+        for (int i = 0; i < array.length; i++){
+            array[i] = input.nextDouble();
+        }
+        input.close();
+
+        List<Double> myNumbers = Arrays.asList(array);
+        Collections.reverse(myNumbers);
+
+        System.out.println("The array in reverse: ");
+        System.out.println(myNumbers);
 
     }
 }
