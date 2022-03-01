2f85cd7d5e3dd3c99f7522e4d677eb5813cc21c2
Jev22 <50078304+Jev22@users.noreply.github.com>
2022-03-01 16:34:57 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int USER_INPUT_NUMBERS = 10;
+
+        Double[] array = new Double[USER_INPUT_NUMBERS];
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter " + USER_INPUT_NUMBERS + " numbers: ");
+
+    }
+}
